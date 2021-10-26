0451913ba46fe12580e97a57bc9ae2f99a4c2935
Denis Porsev <den.porsev@gmail.com>
2021-10-26 16:39:55 +0300

    Experiments for folder streaming

--- reports/ImageFilteringApp/images/draw.py
+++ reports/ImageFilteringApp/images/draw.py
@@ -6,15 +6,20 @@ from matplotlib.ticker import ScalarFormatter
 from matplotlib.ticker import FormatStrFormatter
 
 
-def draw_bar(file, label, axs, position, bar_height):
+def draw_bar(file, label, axs, position, bar_height, streaming):
     '''
     Draws bar chart for information in benchmark result file
     '''
     df = pd.read_csv(file)
 
     alg_means = list(df.mean(axis=0))
-    plt.yticks([r + bar_height for r in range(len(alg_means))],
-               ['CPU', 'CPU_p', 'GPU'])
+    if not streaming:
+        plt.yticks([r + bar_height for r in range(len(alg_means))],
+                   ['CPU', 'CPU_p', 'GPU'])
+    else:
+        alg_means = alg_means[:2]
+        plt.yticks([r + bar_height for r in range(len(alg_means))],
+                   ['seq', 'concur'])
 
     axs.barh(position, alg_means, label=label,
              height=bar_height, edgecolor='white', alpha=0.8)
@@ -22,18 +27,18 @@ def draw_bar(file, label, axs, position, bar_height):
     plt.ioff()
 
 
-def draw_benchmarks(file_list, title, out, logscale):
+def draw_benchmarks(file_list, title, out, logscale, streaming=False):
     '''
     Draws benchmark results from csv files provided with file_list
     '''
     fig = plt.figure()
     axs = plt.axes()
 
-    bar_height = 0.21
+    bar_height = 0.2
     position = np.arange(len(file_list) - 1)
 
     for (file, label) in file_list:
-        draw_bar(file, label, axs, position, bar_height)
+        draw_bar(file, label, axs, position, bar_height, streaming)
         position = [y + bar_height for y in position]
 
     # customizing chart
@@ -71,3 +76,13 @@ if __name__ == "__main__":
                     ('../../../benchmarks/ImageFilteringApp/output/9/1280x853.csv', '9x9'),
                     ('../../../benchmarks/ImageFilteringApp/output/15/1280x853.csv', '15x15')],
                     'Image 1280x853px with different filters', '1280x853.pdf', True)
+
+    draw_benchmarks([('../../../benchmarks/ImageFilteringApp/output/streaming/640x426x10.csv', '10 imgs'),
+                    ('../../../benchmarks/ImageFilteringApp/output/streaming/640x426x100.csv', '100 imgs'),
+                    ('../../../benchmarks/ImageFilteringApp/output/streaming/640x426x1000.csv', '1000 imgs')],
+                    'Comparisson of concurrent and sequantial versions 640x426', 'streaming640x426.pdf', True, True)
+
+    draw_benchmarks([('../../../benchmarks/ImageFilteringApp/output/streaming/_.csv', ''),
+                    ('../../../benchmarks/ImageFilteringApp/output/streaming/5184x3456x10.csv', '10 imgs'),
+                    ('../../../benchmarks/ImageFilteringApp/output/streaming/_.csv', ''), ],
+                    'Comparisson of concurrent and sequantial versions 5184x3456', 'streaming5184x3456.pdf', False, True)
